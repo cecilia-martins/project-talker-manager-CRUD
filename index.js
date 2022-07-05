f95bb224967d09eefc6033d5b6d17099b1abd6d0
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-07-05 10:14:47 -0300

    :sparkles: feat: Adicionado requisito 2 - endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-// Requisito q
+// Requisito 1
 app.get('/talker', async (req, res) => {
   const talker = await fs.read.read();
-
   return res.status(200).send(talker);
+});
+
+// requisito 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await fs.read.read();
+  // const talkerParse = JSON.parse(talker);
+  const talkerId = talker.find((el) => el.id === Number(id));
+  // console.log(talkerId);
+  if (!talkerId) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada' });
+   }
+   return res.status(200).json(talkerId);
 });
 
 app.listen(PORT, () => {
