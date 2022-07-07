644af8450cacb3aa7f773498f557c9eba788f816
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-07-07 12:31:42 -0300

    :bug: fix: Não tinha salvo os arquivos modificados no push anterior :clown_face: - Troquei a randomUUID pela randomString - localmente continua passando tudo :clown_face:

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const routerLogin = express.Router();
-const { randomUUID } = require('crypto');
+const randomstring = require('randomstring');
+
 const { validationEmail, validationPassdword } = require('../middlewares/loginValidations');
 
 routerLogin.post('/', validationEmail, validationPassdword, (req, res) => {
-  const tokenttt = randomUUID().split('-').join('').substring(0, 16);
-
+  const tokenttt = randomstring.generate(16);
   return res.status(200).json({ token: tokenttt });
 });
 
